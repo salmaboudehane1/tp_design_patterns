113a9f3f518e201b47ed851577e3eb7adefd753c
salmaboudehane1 <salma.solo0204@gmail.com>
2024-11-04 08:35:45 +0000

    exe3 ajout main

---
+++ ex3/src/DataAdapter.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataAdapter {
+    private InternalData internalData;
+
+    public DataAdapter(InternalData internalData) {
+        this.internalData = internalData;
+    }
+
+    public Map<String, String> convertToExternalFormat() {
+        Map<String, String> externalData = new HashMap<>();
+        externalData.put("first_name", internalData.getFirstName());
+        externalData.put("last_name", internalData.getLastName());
+        externalData.put("email", internalData.getEmailAddress());
+        return externalData;
+    }
+}
